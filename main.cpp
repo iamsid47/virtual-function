fc668b906d5bea913d57c6f1c4f670c4247c8236
iamsid47 <siddheshkulthe43@gmail.com>
2023-01-22 22:23:09 +0530

    added pointers

--- main.cpp
+++ main.cpp
@@ -4,27 +4,32 @@ using namespace std;
 class Base {
 	public:
 		void show () {
-			cout << "This is a virtual function"<< endl;
+			cout << "This is a virtual function. Function name - show()"<< endl;
 		}
 		
 		void display (int x) {
-			cout << "This is a virtual function" << endl;
+			cout << "This is a virtual function Function name - display()" << endl;
 			cout << "The square of " << x << " is " << x * x << endl;
 		}
 };
 
-class Base {
+class Derived {
 	public:
 		void show () {
-			cout << "This is a function from derived class"<< endl;
+			cout << "This is a function from derived class Function name - show()"<< endl;
 		}
 		
 		void display (int x) {
-			cout << "This is a function from derived class" << endl;
+			cout << "This is a function from derived class Function name - display()" << endl;
 			cout << "The cube of " << x << " is " << x * x << endl;
 		}
 };
 
 int main() {
+	Base obj;
+	Base *ptr = &obj;
+	ptr -> show();
+	ptr -> display (10);
+	
 	return 0;
 }
