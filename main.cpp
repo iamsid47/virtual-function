fdfe6717b9c6605e048f583cf25d45248cbe7a8b
iamsid47 <siddheshkulthe43@gmail.com>
2023-01-22 22:26:09 +0530

    applied concept of virtual function

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 class Base {
 	public:
-		void show () {
-			cout << "This is a virtual function. Function name - show()"<< endl;
+		virtual void show () {
+			cout << "Virtual function. Function name - show()"<< endl;
 		}
 		
-		void display (int x) {
-			cout << "This is a virtual function Function name - display()" << endl;
+		virtual void display (int x) {
+			cout << "Virtual function Function name - display()" << endl;
 			cout << "The square of " << x << " is " << x * x << endl;
 		}
 };
@@ -16,20 +16,25 @@ class Base {
 class Derived: public Base {
 	public:
 		void show () {
-			cout << "This is a function from derived class Function name - show()"<< endl;
+			cout << "Derived class function. Function name - show()"<< endl;
 		}
 		
 		void display (int x) {
-			cout << "This is a function from derived class Function name - display()" << endl;
+			cout << "Derived class function. Function name - display()" << endl;
 			cout << "The cube of " << x << " is " << x * x << endl;
 		}
 };
 
 int main() {
 	Base obj;
+	Derived drobj;
 	Base *ptr = &obj;
 	ptr -> show();
 	ptr -> display (10);
+	
+	ptr = &drobj;
+	ptr -> show();
+	ptr -> display (20);
 	
 	return 0;
 }
