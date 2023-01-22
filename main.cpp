b24c787861916dc7ab85fdb4755b61e8f7c9b86e
iamsid47 <siddheshkulthe43@gmail.com>
2023-01-22 22:21:00 +0530

    created a class and added functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char** argv) {
+class Base {
+	public:
+		void show () {
+			cout << "This is a virtual function"<< endl;
+		}
+		
+		void display (int x) {
+			cout << "This is a virtual function" << endl;
+			cout << "The square of " << x << " is " << x * x << endl;
+		}
+};
+
+class Base {
+	public:
+		void show () {
+			cout << "This is a function from derived class"<< endl;
+		}
+		
+		void display (int x) {
+			cout << "This is a function from derived class" << endl;
+			cout << "The cube of " << x << " is " << x * x << endl;
+		}
+};
+
+int main() {
 	return 0;
 }
